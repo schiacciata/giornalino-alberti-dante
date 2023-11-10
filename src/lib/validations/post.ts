bb8484a011d221467060a9954c4e36293268b733
PADO <62028267+47PADO47@users.noreply.github.com>
2023-11-10 16:15:31 +0100

    🦺 Export schemas type

--- src/lib/validations/post.ts
+++ src/lib/validations/post.ts
@@ -5,9 +5,13 @@ export const postCreateSchema = z.object({
   content: z.string().optional(),
 })
 
+export type postCreateFormData = z.infer<typeof postCreateSchema>;
+
 export const postPatchSchema = z.object({
   title: z.string().min(3).max(128).optional(),
 
   // TODO: Type this properly from editorjs block types?
   content: z.any().optional(),
 })
+
+export type postUpdateFormData = z.infer<typeof postPatchSchema>;
--- src/lib/validations/user.ts
+++ src/lib/validations/user.ts
@@ -3,3 +3,5 @@ import * as z from "zod"
 export const userNameSchema = z.object({
   name: z.string().min(3).max(32),
 })
+
+export type userUpdateFormData = z.infer<typeof userNameSchema>;
