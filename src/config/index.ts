2c7e4f073afb40ebcdb44c230bddcb54464e0ab0
PADO <62028267+47PADO47@users.noreply.github.com>
2023-11-10 16:14:56 +0100

    🔧 Update admins

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ import { env } from "@/env.mjs";
 
 export default {
     debug: env.NODE_ENV !== "production",
-    admins: ['pietro.padovani06@gmail.co'],
+    admins: ['pietro.padovani06@gmail.com'],
 }
