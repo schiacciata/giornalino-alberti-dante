6577eca19cd166b8131f7e2bce603538f2bbef32
PADO <62028267+47PADO47@users.noreply.github.com>
2024-03-08 09:28:33 +0100

    template

---
+++ src/app/[locale]/(misc)/settings/template.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { motion, AnimatePresence } from "framer-motion"
+
+const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+}
+
+export default function Template({ children }: { children: React.ReactNode }) {
+    return (
+        <AnimatePresence>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence >
+    )
+}
