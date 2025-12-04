57d05ead7d1079eef95b6c3be3154104a38859da
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-12-04 14:33:18 +0100

    fix: "use cache";

--- src/lib/auth/user.ts
+++ src/lib/auth/user.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
 const getSession = async (h: Awaited<ReturnType<typeof headers>>) => {
-	"use cache";
+	//"use cache";
 
 	return await auth.api.getSession({
 		headers: h,
