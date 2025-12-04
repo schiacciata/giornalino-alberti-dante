c752439172e35ab74e33ddf5cedcbf6ff1530723
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-12-04 13:59:20 +0100

    fix: serwist

--- src/app/serwist/[path]/route.ts
+++ src/app/serwist/[path]/route.ts
@@ -2,7 +2,7 @@ import { createSerwistRoute } from "@serwist/turbopack";
 
 export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
 	createSerwistRoute({
-		swSrc: "public/sw.js",
+		swSrc: "src/worker/index.ts",
 		nextConfig: {
 			basePath: "/",
 		},
